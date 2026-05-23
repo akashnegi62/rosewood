6f2823391316caac190fa732fde4af60822b5479
akashnegi62 <akashsing215@gmail.com>
2026-05-23 16:56:36 +0530

    Membersec

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Home from "@/components/Home";
 import Destinations from "@/components/Destinations";
+import Membersec from "@/components/Membersec";
 
 function page() {
   return (
     <>
       <Home />
       <Destinations/>
+      <Membersec/>
     </>
   );
 }
