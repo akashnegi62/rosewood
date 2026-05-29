8463bbe98c34fd99a4f0075d58658da58aaeaa40
akashnegi62 <akashsing215@gmail.com>
2026-05-29 16:36:46 +0530

    Club Change

--- components/Club/Imagesec.tsx
+++ components/Club/Imagesec.tsx
@@ -2,21 +2,33 @@
 
 import React from "react";
 import Image from "next/image";
+import Heading from "@/components/Heading";
 
 export default function Imagesec() {
   return (
-    <main className="relative min-h-screen w-full bg-black text-white flex flex-col justify-between overflow-hidden select-none">
-      {/* BACKGROUND IMAGE */}
-      <div className="absolute inset-0 w-full h-full z-0 select-none pointer-events-none">
-        <Image
-          src="/Img/clubelevate.png"
-          alt="Club Elevate"
-          fill
-          priority
-          sizes="100vw"
-          className="object-cover"
+    <>
+      {/* MOBILE HEADING */}
+      <div className="block md:hidden">
+        <Heading
+          title="Club Elevate"
+          subtitle="A World of Privileged Access"
         />
       </div>
-    </main>
+
+      {/* DESKTOP BACKGROUND IMAGE */}
+      <main className="hidden md:flex relative min-h-screen w-full bg-black text-white flex-col justify-between overflow-hidden select-none">
+        {/* BACKGROUND IMAGE */}
+        <div className="absolute inset-0 w-full h-full z-0 select-none pointer-events-none">
+          <Image
+            src="/Img/clubelevate.png"
+            alt="Club Elevate"
+            fill
+            priority
+            sizes="100vw"
+            className="object-cover"
+          />
+        </div>
+      </main>
+    </>
   );
 }
